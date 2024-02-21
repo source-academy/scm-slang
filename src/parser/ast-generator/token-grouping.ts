5facdfc018d23563366172177738ec0da5e02e71
s-kybound <abadkyriel@gmail.com>
2024-02-21 22:37:04 +0800

    Simplify token grouping

--- src/parser/ast-generator/token-grouping.ts
+++ src/parser/ast-generator/token-grouping.ts
@@ -1,7 +1,7 @@
 /**
  * A data structure grouping together elements of the same level of parentheses.
  * The parentheses are included in the grouping.
- * The final intermediate representation before AST generation.
+ * An intermediate representation before AST generation.
  */
 
 import { Token } from "../types/token";
@@ -9,64 +9,108 @@ import { TokenType } from "../types/token-type";
 import { Location, Position } from "../types/location";
 
 export class Group {
-  elements: (Token | Group)[];
-  location: Location;
-  constructor(
-    elements: (Token | Group)[],
-    lastPos?: Position, // Allows for the last position to be specified,
-    // important to preserve the location when truncating groups
-  ) {
+  // Invariants:
+  // - A group must not be empty.
+  // - If a group is not parenthesized, it contains either one element, that is not a group,
+  //   or two elements, of which the first one is not a group but the second one is.
+  // - If a group is parenthesized, it must have matching parentheses.
+
+  readonly elements: (Token | Group)[];
+  readonly location: Location;
+  private constructor(elements: (Token | Group)[]) {
     this.elements = elements;
-    if (!this.validateGroupIntegrity()) {
+    this.location = new Location(this.firstPos(), this.lastPos());
+  }
+
+  /**
+   * A constructor function for a group that enforces group invariants.
+   */
+  public static build(elements: (Token | Group)[]) {
+    // helper function to check if the parentheses match.
+    function matchingParentheses(lParen: Token, rParen: Token) {
+      return (
+        (lParen.type === TokenType.LEFT_PAREN &&
+          rParen.type === TokenType.RIGHT_PAREN) ||
+        (lParen.type === TokenType.LEFT_BRACKET &&
+          rParen.type === TokenType.RIGHT_BRACKET)
+      );
     }
-    this.location = new Location(
-      this.firstPos(),
-      lastPos ? lastPos : this.lastPos(),
-    );
-  }
 
-  // helper function to check if the parentheses match.
-  matchingParentheses(lParen: Token, rParen: Token) {
-    return (
-      (lParen.type === TokenType.LEFT_PAREN &&
-        rParen.type === TokenType.RIGHT_PAREN) ||
-      (lParen.type === TokenType.LEFT_BRACKET &&
-        rParen.type === TokenType.RIGHT_BRACKET)
-    );
-  }
+    // helper function to check if the token is a data type.
+    function isDataType(token: Token) {
+      return (
+        token.type === TokenType.IDENTIFIER ||
+        token.type === TokenType.NUMBER ||
+        token.type === TokenType.STRING ||
+        token.type === TokenType.BOOLEAN
+      );
+    }
 
-  // Check for the case of invalid parentheses.
-  validateGroupIntegrity(): boolean {
-    // if the group is empty, it is not valid.
-    if (this.elements.length === 0) {
+    // helper function to determine if the token is an affector type.
+    // (and the affector type should be the short version).
+    function isShortAffector(token: Token) {
+      return (
+        token.type === TokenType.APOSTROPHE ||
+        token.type === TokenType.BACKTICK ||
+        token.type === TokenType.HASH ||
+        token.type === TokenType.COMMA ||
+        token.type === TokenType.COMMA_AT
+      );
+    }
+
+    // Illegal empty group.
+    if (elements.length === 0) {
+      // This should never happen.
+      // If it does its the implementor's fault.
       throw new Error("Empty group.");
     }
 
-    // get the first and last elements of the group.
-    const firstElement = this.first();
-    const lastElement = this.last();
-    // check if the first element is not a parenthesis.
-    // if so, the group is not bounded by parentheses.
-    if (!(firstElement instanceof Token)) {
-      return true;
-    } else if (
-      !(
-        firstElement.type === TokenType.LEFT_PAREN ||
-        firstElement.type === TokenType.LEFT_BRACKET
-      )
-    ) {
-      return true;
-    } else {
-      // we have confirmed that the first element is a parenthesis.
-      // the last element must also be a parenthesis.
-      if (!(lastElement instanceof Token)) {
-        return false;
-      } else {
-        // we have confirmed that the last element is a token.
-        // the parentheses must match.
-        return this.matchingParentheses(firstElement, lastElement);
+    // If the group is not parenthesized, the first case contains only one element.
+    if (elements.length === 1) {
+      const onlyElement: Group | Token = elements[0];
+
+      if (onlyElement instanceof Group) {
+        // Return the inner group.
+        // Avoid nested groups that are a product of the grouping generation in the parser.
+        // Ensures the single internal element is not a group.
+        return onlyElement;
       }
+
+      // Ensure the single element is a data type by validating its token type.
+      if (!isDataType(onlyElement)) {
+        // This should never happen.
+        // If it does its the implementor's fault.
+        throw new Error("Invalid group.");
+      }
+
+      return new Group(elements);
     }
+
+    // If the group is not parenthesized, the remaining case contains two elements.
+    if (elements.length === 2) {
+      const firstElement = elements[0];
+
+      // Ensure the first element is an affector type.
+      if (firstElement instanceof Token && isShortAffector(firstElement)) {
+        // Ensure the first element is an affector type.
+        return new Group(elements);
+      }
+
+      // If all else fails, use the most generic case. below.
+    }
+
+    // If the group is parenthesized, the parentheses must match.
+    const firstElement = elements[0];
+    const lastElement = elements[elements.length - 1];
+    if (
+      firstElement instanceof Token &&
+      lastElement instanceof Token &&
+      matchingParentheses(firstElement, lastElement)
+    ) {
+      return new Group(elements);
+    }
+
+    throw new Error("Invalid group.");
   }
 
   // Get the first element of the group.
@@ -94,26 +138,36 @@ export class Group {
     return this.elements[this.elements.length - 1];
   }
 
+  lastToken(): Token {
+    const lastElement = this.last();
+    if (lastElement instanceof Token) {
+      return lastElement;
+    } else {
+      return lastElement.lastToken();
+    }
+  }
+
   // Get the ending position of the last element of the group.
   lastPos(): Position {
-    const lastElement = this.last();
-    if (lastElement instanceof Group) {
-      return lastElement.lastPos();
-    } else {
-      return lastElement.endPos;
-    }
+    return this.lastToken().pos;
   }
 
   /**
    * Check if the current group is parenthesized.
    */
   public isParenthesized(): boolean {
-    const firstElement = this.elements[0];
-    const lastElement = this.elements[this.elements.length - 1];
-    if (!(firstElement instanceof Token && lastElement instanceof Token)) {
-      return false;
-    }
-    return this.matchingParentheses(firstElement, lastElement);
+    const firstElement = this.first();
+    const lastElement = this.last();
+
+    // Because of the validation performed by the factory function,
+    // we can assume that as long as the first element is a paranthesis,
+    // the last element is also the corresponding paranthesis.
+
+    return (
+      firstElement instanceof Token &&
+      (firstElement.type === TokenType.LEFT_PAREN ||
+        firstElement.type === TokenType.LEFT_BRACKET)
+    );
   }
 
   /**
@@ -124,16 +178,8 @@ export class Group {
   public unwrap(): (Token | Group)[] {
     const firstElement = this.first();
     const lastElement = this.last();
-    if (
-      firstElement instanceof Token &&
-      lastElement instanceof Token &&
-      ((firstElement.type === TokenType.LEFT_PAREN &&
-        lastElement.type === TokenType.RIGHT_PAREN) ||
-        (firstElement.type === TokenType.LEFT_BRACKET &&
-          lastElement.type === TokenType.RIGHT_BRACKET))
-    ) {
+    if (this.isParenthesized()) {
       return this.elements.slice(1, this.elements.length - 1);
-    } else {
     }
     return this.elements;
   }
@@ -145,29 +191,6 @@ export class Group {
    */
   public length(): number {
     return this.unwrap().length;
-  }
-
-  /**
-   * Add a token to front of group.
-   * @param token The token to be added.
-   */
-  public admit(token: Token): void {
-    this.elements.unshift(token);
-    this.location = new Location(token.pos, this.location.end);
-  }
-
-  /**
-   * Truncate the group to exclude the first n elements.
-   * Ignores parentheses.
-   * Preserves end position.
-   *
-   * Useful for lambda expressions and function definitions.
-   * @param n The number of elements to be truncated.
-   */
-  public truncate(n: number): Group {
-    const unwrapped = this.unwrap();
-    const truncated = unwrapped.splice(n);
-    return new Group(truncated, this.lastPos());
   }
 
   /**
