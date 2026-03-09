b54a94fed121c8bc82409775ecd74bb0c6e27a27
Jaye Jyn <jayejyn06@gmail.com>
2026-03-09 15:01:17 +0800

    Add ESLint v9 flat config: eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+// eslint.config.js
+export default [
+  {
+    files: ["**/*.ts", "**/*.js"],
+    ignores: ["dist/**", "node_modules/**"], // You can add more folders to ignore here
+    languageOptions: {
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json"
+      }
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin")
+    },
+    rules: {
+      // add your rules or leave empty for defaults
+    }
+  }
+];
