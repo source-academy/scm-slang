d596cc7d6e95fc37d2a6ff09735f13af83afe1df
Jaye Jyn <jayejyn06@gmail.com>
2026-03-09 15:23:49 +0800

    Switch ESLint config to static export for flat config CLI compatibility

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,18 @@
-// eslint.config.js
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 
-export default async () => [
+export default [
   {
     files: ["**/*.ts", "**/*.js"],
     ignores: ["dist/**", "node_modules/**"],
     languageOptions: {
-      parser: (await import("@typescript-eslint/parser")).default,
+      parser: tsParser,
     },
     plugins: {
-      "@typescript-eslint": (await import("@typescript-eslint/eslint-plugin"))
-        .default,
+      "@typescript-eslint": tsPlugin,
     },
     rules: {
       // your rules here
-    },
-  },
+    }
+  }
 ];
