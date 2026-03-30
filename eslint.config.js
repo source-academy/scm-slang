28cea2feb8d824b52b4c45c5dc5e71811995d732
Jaye Jyn <jayejyn06@gmail.com>
2026-03-30 17:05:39 +0800

    address requested changes

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,19 @@
-import tsParser from "@typescript-eslint/parser";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tseslint from "typescript-eslint";
 
 export default [
+  { ignores: ["dist/**", "node_modules/**"] },
+  ...tseslint.configs.recommended,
   {
     files: ["**/*.ts", "**/*.js"],
-    ignores: ["dist/**", "node_modules/**"],
-    languageOptions: {
-      parser: tsParser,
-    },
-    plugins: {
-      "@typescript-eslint": tsPlugin,
-    },
     rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "@typescript-eslint/no-namespace": "off",
+      "prefer-const": "off",
+      "no-var": "off",
       // your rules here
     },
   },
